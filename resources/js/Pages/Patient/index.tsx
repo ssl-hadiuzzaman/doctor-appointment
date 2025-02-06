c1c520d110e1a2bdf92f02716a191aaf9531d27f
Hadiuzzaman <ssl.hadiuzzaman@outlook.com>
2025-02-06 12:23:13 +0600

    laravel with react setup

---
+++ resources/js/Pages/Patient/index.tsx
@@ -0,0 +1,15 @@
+import DefaultLayout from '@/Layouts/DefaultLayout'
+import { DefaultDeserializer } from 'node:v8'
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+        <DefaultLayout>
+            hello world
+        </DefaultLayout>
+    </>
+  )
+}
+
+export default index
